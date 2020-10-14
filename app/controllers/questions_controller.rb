57ca3e473582d29a0d5b1d8d695fbd2a9cf297a1
Clement Perier Muzet <barceloneta08003@gmail.com>
2020-10-14 09:06:54 +0200

    bit of styling

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,7 @@ class QuestionsController < ApplicationController
 
     if @question == 'I am going to work'
       @answer = 'Great!'
-    elsif @question.end_with? '?'
+    elsif @question.end_with? '?'   #! other alternative : @question[-1] == '?'
       @answer = 'Silly question, get dressed and go to work!' 
     else
       @answer = "I don't care, get dressed and go to work!"
